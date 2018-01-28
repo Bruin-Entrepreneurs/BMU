16c3c62f294d9020a770fddaf05aad3e05bc4594
Hao Nguyen <haolnguyen7@gmail.com>
2018-01-28 14:12:18 -0800

    got skeleton of user module down

--- BMU/settings.py
+++ BMU/settings.py
@@ -3,7 +3,7 @@ Django settings for BMU project.
 """
 # Attempt to load environment variables
 try:
-    from ..keys import *
+    from .keys import *
 except ImportError:
     pass
 
@@ -12,11 +12,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = '($t9@_*+dd-ez&*ij$@-90bqi56dz($&udz(sbqc4d=*ygq0jx'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'user.user'
 
 # Application definition
 
---
+++ user/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser as _DjangoAbstractUser
+
+
+# DONT WORRY ABOUT THIS - THIS IS DJANGO MAGIC WE CAN HAVE A CUSTOM AUTH USER
+class AbstractUser(_DjangoAbstractUser):
+    class Meta:
+        abstract = True
+
+# Need to do this after the class object is created, because we need _meta access
+AbstractUser._meta.get_field('groups').rel.related_name = 'special_users'
+AbstractUser._meta.get_field('groups').rel.related_query_name = 'special_users'
+
+AbstractUser._meta.get_field('user_permissions').rel.related_name = 'special_users'
+AbstractUser._meta.get_field('user_permissions').rel.related_query_name = 'special_users'
+
+
+class User(AbstractUser):
+    """
+    Model representing a User who can create and join events
+    """
+    username = models.CharField(max_length=50, db_index=True, unique=True, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    display_name = models.CharField(max_length=100, default='')
+    bio = models.CharField(max_length=500, blank=True, default='')
+
+    # events = models.ManyToManyField('event.events', on_delete=models.SET_NULL, related_name='events', null=True)
