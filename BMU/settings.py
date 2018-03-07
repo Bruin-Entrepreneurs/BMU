7c79aa02774a45bd74aa9a8ff57a7459aeb6be69
Hao Nguyen <haolnguyen7@gmail.com>
2018-03-06 18:40:09 -0800

    static

--- BMU/settings.py
+++ BMU/settings.py
@@ -17,7 +17,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.abspath(__file__ + '/../../../')
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 def get_env_setting(setting, default=None):
     """
