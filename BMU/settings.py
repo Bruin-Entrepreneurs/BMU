f63c0881fe0c42dde4e981cd6195e59830c09292
Hao Nguyen <haolnguyen7@gmail.com>
2018-03-19 01:50:56 -0700

    static

--- BMU/settings.py
+++ BMU/settings.py
@@ -14,14 +14,17 @@ except ImportError:
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.abspath(__file__ + '/../../../')
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_ROOT = 'static'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 def get_env_setting(setting, default=None):
     """
