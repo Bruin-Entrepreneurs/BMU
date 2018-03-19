a1c22488e783c96f53f2e9d7e290dfb0453afe47
Hao Nguyen <haolnguyen7@gmail.com>
2018-03-19 01:54:36 -0700

    less static?

--- BMU/settings.py
+++ BMU/settings.py
@@ -23,9 +23,9 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 def get_env_setting(setting, default=None):
     """
