93af636264338150ea145c5ae193385cfb4ff411
Hao Nguyen <haolnguyen7@gmail.com>
2018-04-01 02:17:53 -0700

    event admins

--- apps/event/admin.py
+++ apps/event/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.event.models import EventType, Event
+
+
+class EventTypeAdmin(admin.ModelAdmin):
+    model = EventType
+
+
+class EventAdmin(admin.ModelAdmin):
+    model = Event
+
+
+admin.site.register(EventType, EventTypeAdmin)
+admin.site.register(Event, EventAdmin)
