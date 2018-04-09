95628588ced4009751ffbb3ca9ec5f1d0846928a
Hao Nguyen <haolnguyen7@gmail.com>
2018-04-09 02:04:49 -0700

    push notifs

--- apps/event/views.py
+++ apps/event/views.py
@@ -3,6 +3,12 @@ from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 from rest_framework import status
 
+from exponent_server_sdk import DeviceNotRegisteredError
+from exponent_server_sdk import PushClient
+from exponent_server_sdk import PushMessage
+from exponent_server_sdk import PushResponseError
+from exponent_server_sdk import PushServerError
+
 from apps.helpers.helpers import get_data_field_or_400, get_data_list_or_400
 from apps.event.serializer import EventSerializer, EventTypeSerializer, EventSummarySerializer
 from apps.event.models import Event, EventType
@@ -22,16 +28,18 @@ class EventListCreateView(ListModelMixin, GenericAPIView):
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        user = request.user
+        creator = request.user
+        users = User.objects.all()
         event_type_id = get_data_field_or_400(request, 'event_type_id')
         start_time = get_data_field_or_400(request, 'start_time')
         end_time = get_data_field_or_400(request, 'end_time')
         super_invite_ids = get_data_list_or_400(request, 'super_invite_ids')
         description = get_data_field_or_400(request, 'description')
 
+        event_type = EventType.objects.get(pk=int(event_type_id))
         event = Event.objects.create(
-            creator=request.user,
-            event_type=EventType.objects.get(pk=int(event_type_id)),
+            creator=creator,
+            event_type=event_type,
             # start_time=start_time,
             # end_time=end_time,
             description=description
@@ -40,8 +48,20 @@ class EventListCreateView(ListModelMixin, GenericAPIView):
         for super_invite_id in super_invite_ids:
             super_invited_user = User.objects.get(pk=int(super_invite_id))
             event.super_invited.add(super_invited_user)
-        event.accepted.add(user)
+        event.accepted.add(creator)
         event.save()
+
+        # Invite all users with a push notification
+        push_client = PushClient()
+        for user in users:
+            try:
+                response = push_client.publish(
+                    PushMessage(to=user.notification_token,
+                                title=event_type.name,
+                                body='You\'ve been invited by ' + creator + '!',
+                                data=''))
+            except:
+                pass
 
         return Response(data=EventSerializer(event).data, status=status.HTTP_201_CREATED)
 
